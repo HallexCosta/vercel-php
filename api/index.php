858a94fd27efe8c511f35494d649138ca1c57921
Hállex da Silva Costa <55293671+HallexCosta@users.noreply.github.com>
2024-09-22 00:17:32 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,4 +4,5 @@
 header('context-type: text/html;charset=utf-8');
 // echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
 
+<h1>Hállex Costa</h1>
 require_once __DIR__ . '/../index.html';
